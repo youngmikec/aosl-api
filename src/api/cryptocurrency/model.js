7cc777d31732b47d74d2610a5fad94884de6b9a2
youngmikec <michaelozor15@gmail.com>
2023-03-03 15:14:44 +0100

    modified crypto,airtime,giftcard schema

--- src/api/cryptocurrency/model.js
+++ src/api/cryptocurrency/model.js
@@ -1,123 +1,134 @@
-import mongoose from 'mongoose';
-import joi from 'joi';
+import mongoose from "mongoose";
+import joi from "joi";
 
-import { AIRTIME, DATABASE } from '../../constant/index.js';
-
-
+import { AIRTIME, DATABASE } from "../../constant/index.js";
 
 const { Schema } = mongoose;
 const { ObjectId } = Schema.Types;
 
 export const validateCreate = joi.object({
-    name: joi.string().trim().required(),
-    shortName: joi.string().trim().required(),
-    rate: joi.number().required(),
-    cryptoImage: joi.string().optional(),
-    barcode: joi.string().optional(),
-    walletAddress: joi.string().trim().required(),
-    bankName: joi.string().trim().required(),
-    accountName: joi.string().trim().required(),
-    accountNumber: joi.string().trim().required(),
-    exchangePlatform: joi.string().trim().required(),
-    networks: joi.array().items(
-        joi.object({
-            networkName: joi.string().required(),
-            networkId: joi.string().optional()
-        })
-    ).optional(),
-    paymentSteps: joi.array().items(
-        joi.object({
-            title: joi.string().required(),
-            description: joi.string().required()
-        })
-    ).optional(),
-    paymentDescription: joi.string().optional(),
-    createdBy: joi.string()
+  name: joi.string().trim().required(),
+  shortName: joi.string().trim().required(),
+  rate: joi.number().required(),
+  cryptoImage: joi.string().optional(),
+  barcode: joi.string().optional(),
+  walletAddress: joi.string().trim().required(),
+  bankName: joi.string().trim().required(),
+  accountName: joi.string().trim().required(),
+  accountNumber: joi.string().trim().required(),
+  exchangePlatform: joi.string().trim().required(),
+  networks: joi
+    .array()
+    .items(
+      joi.object({
+        networkName: joi.string().required(),
+        networkId: joi.string().optional(),
+      })
+    )
+    .optional(),
+  paymentSteps: joi
+    .array()
+    .items(
+      joi.object({
+        title: joi.string().required(),
+        description: joi.string().required(),
+      })
+    )
+    .optional(),
+  paymentDescription: joi.string().optional(),
+  createdBy: joi
+    .string()
     .regex(DATABASE.OBJECT_ID_REGEX, "valid objectID")
-    .optional()
-})
+    .optional(),
+});
 
 export const validateUpdate = joi.object({
-    name: joi.string().trim().optional(),
-    shortName: joi.string().trim().optional(),
-    rate: joi.number().optional(),
-    cryptoImage: joi.string().optional(),
-    barcode: joi.string().optional(),
-    walletAddress: joi.string().trim().optional(),
-    bankName: joi.string().trim().optional(),
-    accountName: joi.string().trim().optional(),
-    accountNumber: joi.string().trim().optional(),
-    exchangePlatform: joi.string().trim().optional(),
-    networks: joi.array().items(
-        joi.object({
-            networkName: joi.string().required(),
-            networkId: joi.string().optional()
-        })
-    ).optional(),
-    paymentSteps: joi.array().items(
-        joi.object({
-            title: joi.string().required(),
-            description: joi.string().required()
-        })
-    ).optional(),
-    paymentDescription: joi.string().optional(),
-    updatedBy: joi.string()
+  name: joi.string().trim().optional(),
+  shortName: joi.string().trim().optional(),
+  rate: joi.number().optional(),
+  cryptoImage: joi.string().optional(),
+  barcode: joi.string().optional(),
+  walletAddress: joi.string().trim().optional(),
+  bankName: joi.string().trim().optional(),
+  accountName: joi.string().trim().optional(),
+  accountNumber: joi.string().trim().optional(),
+  exchangePlatform: joi.string().trim().optional(),
+  networks: joi
+    .array()
+    .items(
+      joi.object({
+        networkName: joi.string().required(),
+        networkId: joi.string().optional(),
+      })
+    )
+    .optional(),
+  paymentSteps: joi
+    .array()
+    .items(
+      joi.object({
+        title: joi.string().required(),
+        description: joi.string().required(),
+      })
+    )
+    .optional(),
+  status: joi.string().valid("ACTIVE", "DEACTIVATED").optional(),
+  paymentDescription: joi.string().optional(),
+  updatedBy: joi
+    .string()
     .regex(DATABASE.OBJECT_ID_REGEX, "valid objectID")
-    .optional()
-})
-
+    .optional(),
+});
 
 export const schema = {
-    name: { type: String, trim: true, unique: true, required: true  },
-    shortName: { type: String, trim: true },
-    walletAddress: { type: String, trim: true },
-    exchangePlatform: { type: String, trim: true },
-    bankName: { type: String, trim: true, select: true },
-    accountName: { type: String, trim: true, select: true },
-    accountNumber: { type: String, trim: true, select: true },
-    code: { type: String, trim: true },
-    rate: { type: Number, default: 1, select: true },
-    cryptoImage: { 
-        type: String, 
-        select: true,
+  name: { type: String, trim: true, unique: true, required: true },
+  shortName: { type: String, trim: true },
+  walletAddress: { type: String, trim: true },
+  exchangePlatform: { type: String, trim: true },
+  bankName: { type: String, trim: true, select: true },
+  accountName: { type: String, trim: true, select: true },
+  accountNumber: { type: String, trim: true, select: true },
+  code: { type: String, trim: true },
+  rate: { type: Number, default: 1, select: true },
+  cryptoImage: {
+    type: String,
+    select: true,
+  },
+  barcode: {
+    type: String,
+    select: true,
+  },
+  networks: [
+    {
+      networkName: { type: String, select: true },
+      networkId: { type: String, select: true },
     },
-    barcode: { 
-        type: String, 
-        select: true,
-    },
-    networks: [
-        {
-          networkName: { type: String, select: true },
-          networkId: { type: String, select: true }
-        }
-        
-    ],
+  ],
 
-    status: { 
-        type: String, 
-        enum: Object.values(AIRTIME.STATUS),
-        default: 'DEACTIVATED', 
-        required: true, 
-        select: true
-    },
+  status: {
+    type: String,
+    enum: Object.values(AIRTIME.STATUS),
+    default: "ACTIVE",
+    required: true,
+    select: true,
+  },
 
-    paymentDescription: { type: String },
-    paymentSteps: { type: Array, default: [], select: true },
+  paymentDescription: { type: String },
+  paymentSteps: { type: Array, default: [], select: true },
 
-    createdBy: { type: ObjectId, ref: "Users", required: true, select: true },
-    updatedBy: { type: ObjectId, ref: "Users", select: false },
-    deleted: { type: Boolean, default: false, select: false },
-    deletedAt: { type: Date, select: false },
-    deletedBy: { type: ObjectId, select: false },
-}
+  createdBy: { type: ObjectId, ref: "Users", required: true, select: true },
+  updatedBy: { type: ObjectId, ref: "Users", select: false },
+  deleted: { type: Boolean, default: false, select: false },
+  deletedAt: { type: Date, select: false },
+  deletedBy: { type: ObjectId, select: false },
+};
 
-const options = DATABASE.OPTIONS
+const options = DATABASE.OPTIONS;
 
 const newSchema = new Schema(schema, options);
 
-newSchema.index({name: 1}, { unique: true});
-newSchema.set('collection', 'cryptocurrency');
+newSchema.index({ name: 1 }, { unique: true });
+newSchema.set("collection", "cryptocurrency");
 
-const Cryptocurrency = mongoose.model('Cryptocurrency', newSchema);
+const Cryptocurrency = mongoose.model("Cryptocurrency", newSchema);
 
 export default Cryptocurrency;
--- src/api/giftcard/model.js
+++ src/api/giftcard/model.js
@@ -1,128 +1,147 @@
-import mongoose from 'mongoose';
-import joi from 'joi';
-import { DATABASE, GIFTCARD } from '../../constant/index.js';
-
+import mongoose from "mongoose";
+import joi from "joi";
+import { DATABASE, GIFTCARD } from "../../constant/index.js";
 
 const { Schema } = mongoose;
 const { ObjectId } = Schema.Types;
 
-
 export const validateCreate = joi.object({
-    name: joi.string().trim().required(),
-    shortName: joi.string().trim().required(),
-    type: joi.string().trim().valid(...Object.values(GIFTCARD.TYPE)).required(),
-    rate: joi.number().required(),
-    giftcardImage: joi.string().optional(),
-    barcode: joi.string().optional(),
-    walletAddress: joi.string().trim().required(),
-    bankName: joi.string().trim().required(),
-    accountName: joi.string().trim().required(),
-    accountNumber: joi.string().trim().required(),
-    exchangePlatform: joi.string().trim().required(),
-    currencies: joi.array().items(
-        joi.object({
-            rate: joi.number().required(),
-            name: joi.string().optional()
-        })
-    ).optional(),
-    paymentSteps: joi.array().items(
-        joi.object({
-            title: joi.string().required(),
-            description: joi.string().required()
-        })
-    ).optional(),
-    paymentDescription: joi.string().optional(),
-    createdBy: joi.string()
+  name: joi.string().trim().required(),
+  shortName: joi.string().trim().required(),
+  type: joi
+    .string()
+    .trim()
+    .valid(...Object.values(GIFTCARD.TYPE))
+    .required(),
+  rate: joi.number().required(),
+  giftcardImage: joi.string().optional(),
+  barcode: joi.string().optional(),
+  walletAddress: joi.string().trim().required(),
+  bankName: joi.string().trim().required(),
+  accountName: joi.string().trim().required(),
+  accountNumber: joi.string().trim().required(),
+  exchangePlatform: joi.string().trim().required(),
+  currencies: joi
+    .array()
+    .items(
+      joi.object({
+        rate: joi.number().required(),
+        name: joi.string().optional(),
+      })
+    )
+    .optional(),
+  paymentSteps: joi
+    .array()
+    .items(
+      joi.object({
+        title: joi.string().required(),
+        description: joi.string().required(),
+      })
+    )
+    .optional(),
+  paymentDescription: joi.string().optional(),
+  createdBy: joi
+    .string()
     .regex(DATABASE.OBJECT_ID_REGEX, "valid objectID")
-    .optional()
-})
+    .optional(),
+});
 
 export const validateUpdate = joi.object({
-    name: joi.string().trim().optional(),
-    shortName: joi.string().trim().optional(),
-    type: joi.string().trim().valid(...Object.values(GIFTCARD.TYPE)).optional(),
-    rate: joi.number().optional(),
-    giftcardImage: joi.string().optional(),
-    barcode: joi.string().optional(),
-    walletAddress: joi.string().trim().optional(),
-    bankName: joi.string().trim().optional(),
-    accountName: joi.string().trim().optional(),
-    accountNumber: joi.string().trim().optional(),
-    exchangePlatform: joi.string().trim().optional(),
-    currencies: joi.array().items(
-        joi.object({
-            rate: joi.number().required(),
-            name: joi.string().optional()
-        })
-    ).optional(),
-    paymentSteps: joi.array().items(
-        joi.object({
-            title: joi.string().required(),
-            description: joi.string().required()
-        })
-    ).optional(),
-    paymentDescription: joi.string().optional(),
-    updatedBy: joi.string()
+  name: joi.string().trim().optional(),
+  shortName: joi.string().trim().optional(),
+  type: joi
+    .string()
+    .trim()
+    .valid(...Object.values(GIFTCARD.TYPE))
+    .optional(),
+  rate: joi.number().optional(),
+  giftcardImage: joi.string().optional(),
+  barcode: joi.string().optional(),
+  walletAddress: joi.string().trim().optional(),
+  bankName: joi.string().trim().optional(),
+  accountName: joi.string().trim().optional(),
+  accountNumber: joi.string().trim().optional(),
+  exchangePlatform: joi.string().trim().optional(),
+  currencies: joi
+    .array()
+    .items(
+      joi.object({
+        rate: joi.number().required(),
+        name: joi.string().optional(),
+      })
+    )
+    .optional(),
+  paymentSteps: joi
+    .array()
+    .items(
+      joi.object({
+        title: joi.string().required(),
+        description: joi.string().required(),
+      })
+    )
+    .optional(),
+  status: joi.string().valid("ACTIVE", "DEACTIVATED").optional(),
+  paymentDescription: joi.string().optional(),
+  updatedBy: joi
+    .string()
     .regex(DATABASE.OBJECT_ID_REGEX, "valid objectID")
-    .optional()
-})
-
+    .optional(),
+});
 
 export const schema = {
-    name: { type: String, select: true },
-    code: { type: String, select: true },
-    rate: { type: Number, default: 1, select: true },
-    bankName: { type: String, trim: true, select: true },
-    accountName: { type: String, trim: true, select: true },
-    accountNumber: { type: String, trim: true, select: true },
-    giftcardImage: { 
-        type: String, 
-        select: true,
-    },
-    barcode: { 
-        type: String, 
-        select: true,
-    },
-    type: { 
-        type: String, 
-        enum: Object.values(GIFTCARD.TYPE),
-        default: GIFTCARD.TYPE.PHYSICAL, 
-        required: true, 
-        select: true
-    },
+  name: { type: String, select: true },
+  code: { type: String, select: true },
+  rate: { type: Number, default: 1, select: true },
+  bankName: { type: String, trim: true, select: true },
+  accountName: { type: String, trim: true, select: true },
+  accountNumber: { type: String, trim: true, select: true },
+  giftcardImage: {
+    type: String,
+    select: true,
+  },
+  barcode: {
+    type: String,
+    select: true,
+  },
+  type: {
+    type: String,
+    enum: Object.values(GIFTCARD.TYPE),
+    default: GIFTCARD.TYPE.PHYSICAL,
+    required: true,
+    select: true,
+  },
 
-    currencies: [
-        {
-          rate: { type: Number, select: true },
-          name: { type: String, select: true },
-        }
-        
-    ],
-
-    status: { 
-        type: String, 
-        enum: Object.values(GIFTCARD.STATUS),
-        default: 'DEACTIVATED', 
-        required: true, 
-        select: true
+  currencies: [
+    {
+      rate: { type: Number, select: true },
+      name: { type: String, select: true },
     },
+  ],
 
-    paymentDescription: { type: String },
-    paymentSteps: { type: Array, default: [], select: true },
+  status: {
+    type: String,
+    enum: Object.values(GIFTCARD.STATUS),
+    default: "ACTIVE",
+    required: true,
+    select: true,
+  },
 
-    createdBy: { type: ObjectId, ref: "Users", required: true, select: true },
-    updatedBy: { type: ObjectId, ref: "Users", select: false },
-    deleted: { type: Boolean, default: false, select: false },
-    deletedAt: { type: Date, select: false },
-    deletedBy: { type: ObjectId, select: false },
-}
+  paymentDescription: { type: String },
+  paymentSteps: { type: Array, default: [], select: true },
+
+  createdBy: { type: ObjectId, ref: "Users", required: true, select: true },
+  updatedBy: { type: ObjectId, ref: "Users", select: false },
+  deleted: { type: Boolean, default: false, select: false },
+  deletedAt: { type: Date, select: false },
+  deletedBy: { type: ObjectId, select: false },
+};
 
 const options = DATABASE.OPTIONS;
 
 const newSchema = new Schema(schema, options);
 
-newSchema.set('collection', 'giftcards');
+newSchema.set("collection", "giftcards");
 
-const Giftcard = mongoose.model('Giftcard', newSchema);
+const Giftcard = mongoose.model("Giftcard", newSchema);
 
 export default Giftcard;
